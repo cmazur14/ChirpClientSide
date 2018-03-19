e137d006e2fc62776690e61048264a86026f98c3
CJ Mazur <mazucj22@wclive.westminster.edu>
2018-03-19 10:35:26 -0400

    this commit is adding the AndroidStudio files as needed

--- app/src/main/java/cjmazur/homework/cs383/chirp/SingleFragmentActivity.java
+++ app/src/main/java/cjmazur/homework/cs383/chirp/SingleFragmentActivity.java
@@ -9,5 +9,7 @@ public class SingleFragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_fragment);
+
+        //now there's a comment in it!
     }
 }
