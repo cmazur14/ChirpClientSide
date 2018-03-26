6bada167339772abeb0a11279e4b5aaf4980bc48
Windsor <dwinds53@gmail.com>
2018-03-26 13:52:19 -0400

    Redid layouts and docs for activities. Also added RecyclerView and CardView dependencies

--- app/src/main/java/cjmazur/homework/cs383/chirp/activities/LoginActivity.java
+++ app/src/main/java/cjmazur/homework/cs383/chirp/activities/LoginActivity.java
@@ -1,16 +1,28 @@
-package cjmazur.homework.cs383.chirp;
+package cjmazur.homework.cs383.chirp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SingleFragmentActivity extends AppCompatActivity {
+import cjmazur.homework.cs383.chirp.R;
+
+/**
+ * @author David Windsor
+ * @author Chris Mazur
+ * @author Devin Cheplic
+ *
+ * @since 3/26/18
+ *
+ * Activity for handling the login and registration of users
+ * When a login file is present the activity will auto login the user, otherwise it will prompt a login
+ * and if needed guide them through registration
+ */
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_fragment);
-
-        //now there's a comment in it!
+        setContentView(R.layout.login);
     }
 
 //    private void sendListOfUsersRequest() {
